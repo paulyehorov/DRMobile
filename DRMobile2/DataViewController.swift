b5d24821bd897e5f52e76e1b0b5f76b96b4a27ca
Paul Yehorov <paul.yehorov@gmail.com>
2016-11-10 14:37:54 +0200

    Initial Commit

---
+++ DRMobile2/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  DRMobile2
+//
+//  Created by Paul Yehorov on 11/10/16.
+//  Copyright © 2016 Paul Yehorov. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
